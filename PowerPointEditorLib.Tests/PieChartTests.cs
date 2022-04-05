414ad61c6fd55a23649f5c49472e7d7cbb732f98
Chris Felstead <chris.felstead@futuressport.com>
2022-04-05 11:32:40 +0100

    Added the ability to work with the values inside a pie chart

---
+++ PowerPointEditorLib.Tests/PieChartTests.cs
@@ -0,0 +1,96 @@
+﻿using DocumentFormat.OpenXml.Drawing.Charts;
+
+namespace PowerPointEditorLib.Tests;
+
+[Collection("UsesFile-Test")]
+public class PieChartTests
+{
+    [Fact]
+    public void FindPieChart_NoCharts_ThrowsError()
+    {
+        string examplePpt = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,
+                                         "ppt-examples",
+                                         "test.pptx");
+
+        var ppt = new PowerPointPresentation(examplePpt, true);
+
+        Assert.Throws<NullReferenceException>(() => ppt.ForSlide(1).FindPieChart(1));
+
+        ppt.Close();
+    }
+
+    [Fact]
+    public void FindPieChart_ChartNumberIncorrect_ThrowsError()
+    {
+        string examplePpt = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,
+                                         "ppt-examples",
+                                         "test.pptx");
+
+        var ppt = new PowerPointPresentation(examplePpt, true);
+
+        Assert.Throws<NullReferenceException>(() => ppt.ForSlide(4).FindPieChart(2));
+
+        ppt.Close();
+    }
+
+    [Fact]
+    public void FindPieChart_ChartReturned()
+    {
+        string examplePpt = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,
+                                         "ppt-examples",
+                                         "test.pptx");
+
+        var ppt = new PowerPointPresentation(examplePpt, true);
+
+        Assert.NotNull(ppt.ForSlide(4).FindPieChart(1));
+
+        ppt.Close();
+    }
+
+    [Fact]
+    public void FindSeries_SeriesReturned()
+    {
+        string examplePpt = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,
+                                         "ppt-examples",
+                                         "test.pptx");
+
+        var ppt = new PowerPointPresentation(examplePpt, true);
+
+        Assert.NotNull(ppt.ForSlide(4).FindPieChart(1).FindSeries());
+
+        ppt.Close();
+    }
+
+    [Fact]
+    public void ReplaceValuesWith_MakesChange()
+    {
+        string examplePpt = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,
+                                         "ppt-examples",
+                                         "test.pptx");
+
+        var ppt = new PowerPointPresentation(examplePpt, true);
+
+        PieChartSeries series = ppt.ForSlide(4).FindPieChart(1).FindSeries();
+        string beforeXml = series.InnerXml;
+        series.ReplaceValuesWith(new List<string> { "2", "4", "4", "2" });
+        string afterXml = series.InnerXml;
+
+        Assert.NotEqual(beforeXml, afterXml);
+
+        ppt.Close();
+    }
+
+    [Fact]
+    public void ReplaceValuesWith_WrongNumberOfValues_ThrowsErrors()
+    {
+        string examplePpt = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,
+                                         "ppt-examples",
+                                         "test.pptx");
+
+        var ppt = new PowerPointPresentation(examplePpt, true);
+
+        Assert.Throws<ArgumentException>(() => ppt.ForSlide(4).FindPieChart(1).FindSeries().ReplaceValuesWith(new List<string> { "2", "4", "4" }));
+
+        ppt.Close();
+    }
+}
---
+++ PowerPointEditorLib/PieChartExtensions.cs
@@ -0,0 +1,54 @@
+﻿using DocumentFormat.OpenXml.Drawing.Charts;
+
+namespace PowerPointEditorLib;
+
+public static class PieChartExtensions
+{
+    public static PieChart FindPieChart(this SlidePart slidePart, int chartNumber)
+    {
+        IEnumerable<ChartPart> chartPart = slidePart.ChartParts.Where(cp => cp.ChartSpace.Descendants<PieChart>().Any());
+
+        if (chartPart.Any() == false)
+            throw new NullReferenceException("Cannot find any pie charts on the slide");
+
+        
+        
+        ChartPart? selectedChartPart = chartPart.ElementAtOrDefault(chartNumber - 1);
+
+        if (selectedChartPart is null)
+            throw new NullReferenceException($"Cannot find a pie chart at the given position ('{chartNumber}')");
+
+
+
+        return selectedChartPart.ChartSpace.Descendants<PieChart>().First();
+    }
+
+    public static PieChartSeries FindSeries(this PieChart pieChart)
+    {
+        PieChartSeries? series = pieChart.Elements<PieChartSeries>().FirstOrDefault();
+
+        if (series is null)
+            throw new NullReferenceException($"No pie chart series has been found");
+
+        return series;
+    }
+
+    public static void ReplaceValuesWith(this PieChartSeries series, List<string> newValues)
+    {
+        var values = series.GetFirstChild<Values>() ?? throw new NullReferenceException("Cannot read chart elements (values)");
+        var numRef = values.GetFirstChild<NumberReference>() ?? throw new NullReferenceException("Cannot read chart elements (numberReference)");
+        var numCache = numRef.GetFirstChild<NumberingCache>() ?? throw new NullReferenceException("Cannot read chart elements (numberingCache)");
+
+        int chartValuesCount = numCache.Elements<NumericPoint>().Count();
+        if (chartValuesCount != newValues.Count())
+            throw new ArgumentException($"The number of values provided for the chart edit ({newValues.Count()}) does not match the number of values used in the chart ({chartValuesCount})");
+
+        int valuePosition = -1;
+        foreach (var np in numCache.Elements<NumericPoint>())
+        {
+            valuePosition++;
+            var nv = np.GetFirstChild<NumericValue>() ?? throw new NullReferenceException("Cannot read chart elements (numericValue)");
+            nv.Text = newValues[valuePosition];
+        }
+    }
+}
