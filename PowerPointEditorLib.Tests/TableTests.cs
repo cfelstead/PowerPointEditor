01337775a6f97e161e67d1148f707b1be1f80d2b
Chris Felstead <chris.felstead@futuressport.com>
2022-05-06 12:28:29 +0100

    Added the ability to find tables and change text contents

---
+++ PowerPointEditorLib.Tests/TableTests.cs
@@ -0,0 +1,130 @@
+﻿using DocumentFormat.OpenXml.Drawing;
+
+namespace PowerPointEditorLib.Tests;
+
+[Collection("UsesFile-Test")]
+public class TableTests
+{
+    [Fact]
+    public void FindTable_NoTable_ThrowsError()
+    {
+        string examplePpt = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory,
+                                         "ppt-examples",
+                                         "test.pptx");
+
+        var ppt = new PowerPointPresentation(examplePpt, true);
+
+        Assert.Throws<NullReferenceException>(() => ppt.ForSlide(1).FindTable(1));
+
+        ppt.Close();
+    }
+
+    [Fact]
+    public void FindTable_TableNumberIncorrect_ThrowsError()
+    {
+        string examplePpt = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory,
+                                         "ppt-examples",
+                                         "test.pptx");
+
+        var ppt = new PowerPointPresentation(examplePpt, true);
+
+        Assert.Throws<NullReferenceException>(() => ppt.ForSlide(5).FindTable(2));
+
+        ppt.Close();
+    }
+
+    [Fact]
+    public void FindTable_TableReturned()
+    {
+        string examplePpt = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory,
+                                         "ppt-examples",
+                                         "test.pptx");
+
+        var ppt = new PowerPointPresentation(examplePpt, true);
+
+        Assert.NotNull(ppt.ForSlide(5).FindTable(1));
+
+        ppt.Close();
+    }
+
+    [Fact]
+    public void FindRow_RowNumberIncorrect_ThrowsError()
+    {
+        string examplePpt = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory,
+                                         "ppt-examples",
+                                         "test.pptx");
+
+        var ppt = new PowerPointPresentation(examplePpt, true);
+
+        Table tbl = ppt.ForSlide(5).FindTable(1);
+
+        Assert.Throws<NullReferenceException>(() => tbl.FindRow(99));
+
+        ppt.Close();
+    }
+
+    [Fact]
+    public void FindRow_RowReturned()
+    {
+        string examplePpt = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory,
+                                         "ppt-examples",
+                                         "test.pptx");
+
+        var ppt = new PowerPointPresentation(examplePpt, true);
+
+        Assert.NotNull(ppt.ForSlide(5).FindTable(1).FindRow(2));
+
+        ppt.Close();
+    }
+
+    [Fact]
+    public void FindCell_CellNumberIncorrect_ThrowsError()
+    {
+        string examplePpt = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory,
+                                         "ppt-examples",
+                                         "test.pptx");
+
+        var ppt = new PowerPointPresentation(examplePpt, true);
+
+        TableRow row = ppt.ForSlide(5).FindTable(1).FindRow(2);
+
+        Assert.Throws<NullReferenceException>(() => row.FindCell(99));
+
+        ppt.Close();
+    }
+
+    [Fact]
+    public void FindCell_CellReturned()
+    {
+        string examplePpt = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory,
+                                         "ppt-examples",
+                                         "test.pptx");
+
+        var ppt = new PowerPointPresentation(examplePpt, true);
+
+        Assert.NotNull(ppt.ForSlide(5).FindTable(1).FindRow(2).FindCell(2));
+
+        ppt.Close();
+    }
+
+    [Fact]
+    public void Cell_TextChanged()
+    {
+        string examplePpt = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory,
+                                         "ppt-examples",
+                                         "test.pptx");
+
+        var ppt = new PowerPointPresentation(examplePpt, true);
+
+
+        TableCell cell = ppt.ForSlide(5).FindTable(1).FindRow(2).FindCell(1);
+        string beforeCellInnerText = cell.InnerText;
+
+        cell.ReplaceCellText("S7 Puebla R8", "Race 01");
+        string afterCellInnerText = cell.InnerText;
+
+        Assert.NotEqual(beforeCellInnerText, afterCellInnerText);
+
+        ppt.Close();
+    }
+}
---
+++ PowerPointEditorLib/TableExtensions.cs
@@ -0,0 +1,102 @@
+﻿using DocumentFormat.OpenXml.Drawing;
+
+namespace PowerPointEditorLib;
+
+public static class TableExtensions
+{
+    public static Table FindTable(this SlidePart slidePart, int tableNumber)
+    {
+        IEnumerable<Table> tables = slidePart.Slide.Descendants<Table>();
+        
+        if (tables.Any() == false)
+            throw new NullReferenceException("Cannot find any tables on the slide");
+
+
+        if (tableNumber > tables.Count())
+            throw new NullReferenceException($"Cannot find a table at the given position ('{tableNumber}')");
+
+
+        Table tbl = tables.ElementAt(tableNumber - 1);
+        
+        if (tbl is null)
+            throw new NullReferenceException($"Cannot find a table at the given position ('{tableNumber}')");
+
+
+
+        return tbl;
+    }
+
+    public static TableRow FindRow(this Table table, int rowNumber)
+    {
+        IEnumerable<TableRow> rows = table.Descendants<TableRow>();
+
+        if (rows.Any() == false)
+            throw new NullReferenceException("Cannot find any rows in this table");
+
+
+        if (rowNumber > rows.Count())
+            throw new NullReferenceException($"Cannot find a row at the given position ('{rowNumber}')");
+
+
+        TableRow row = rows.ElementAt(rowNumber - 1);
+
+        if (row is null)
+            throw new NullReferenceException($"Cannot find a row at the given position ('{rowNumber}')");
+
+
+
+        return row;
+    }
+
+    public static TableCell FindCell(this TableRow row, int cellNumber)
+    {
+        IEnumerable<TableCell> cells = row.Descendants<TableCell>();
+
+        if (cells.Any() == false)
+            throw new NullReferenceException("Cannot find any cells in this row");
+
+
+        if (cellNumber > cells.Count())
+            throw new NullReferenceException($"Cannot find a cell at the given position ('{cellNumber}')");
+
+
+        TableCell cell = cells.ElementAt(cellNumber - 1);
+
+        if (cell is null)
+            throw new NullReferenceException($"Cannot find a cell at the given position ('{cellNumber}')");
+
+
+
+        return cell;
+    }
+
+    public static void ReplaceCellText(this TableCell cell, string searchForText, string replaceWithText)
+    {
+        List<OpenXmlDrawing.Paragraph> paragraphs = new();
+        foreach (var textBody in cell.Descendants<OpenXmlDrawing.TextBody>())
+        {
+            foreach (var paragraph in textBody.Descendants<OpenXmlDrawing.Paragraph>())
+            {
+                paragraphs.Add(paragraph);
+            }
+        }
+
+
+
+        foreach (OpenXmlDrawing.Paragraph para in paragraphs)
+        {
+            if (para.InnerText.Contains(searchForText))
+            {
+                var runInParagraph = para.Descendants<DocumentFormat.OpenXml.Drawing.Run>();
+                foreach (var run in runInParagraph)
+                {
+                    if (run is not null
+                        && run.Text is not null)
+                    {
+                        run.Text.Text = run.Text.Text.Replace(searchForText, replaceWithText);
+                    }
+                }
+            }
+        }
+    }
+}
